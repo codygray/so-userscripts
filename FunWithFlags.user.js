1ab43917d13f4ce726ab8fa15d9236aec756b230
Cody Gray <cody@codygray.com>
2020-07-05 04:16:42 -0700

    Add FunWithFlags; minor updates to SuperShotgun.

---
+++ FunWithFlags.user.js
@@ -0,0 +1,133 @@
+// ==UserScript==
+// @name         Fun With Flags
+// @description  Miscellaneous improvements to the UX for the moderator flag dashboard.
+// @version      0.1.0
+// @author       Cody Gray
+// @homepage     https://github.com/codygray/so-userscripts
+//
+// @updateURL    https://github.com/codygray/so-userscripts/raw/master/FunWithFlags.user.js
+// @downloadURL  https://github.com/codygray/so-userscripts/raw/master/FunWithFlags.user.js
+//
+// @include      */admin/dashboard*
+// ==/UserScript==
+
+(function()
+{
+    'use strict';
+
+    // Moderator check
+    if (typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator) { return; }
+
+    const fkey = StackExchange.options.user.fkey;
+
+
+    function onPageLoad()
+    {
+        // When opening the "decline" options, pre-select the default reason and focus the submit button. */
+        $('.js-resolve-action[data-type="decline"]').click(function(evt)
+        {
+            const flagOpts = $(this).closest('.js-post-flag-group, .js-post-flag-options');
+            setTimeout(() =>
+            {
+                const opt = flagOpts.find('input[name="dismiss-reason"][value="2"]:visible').get(0);
+                if (opt)
+                {
+                    opt.click();
+                }
+
+                const btn = flagOpts.find('.js-submit-btn');
+                if (btn)
+                {
+                    btn.focus();
+                }
+            }, 100);
+        });
+    }
+
+    function appendStyles()
+    {
+        const styles = `
+<style>
+/* Compress the flagged post boxes to save screen space. */
+.js-flagged-post {
+   margin: 24px 12px 0 24px !important;
+}
+
+/* Make visited posts a little less opaque. */
+.visited-post {
+    opacity: 0.7;
+}
+
+/* Make the [edited] badge stand out more. */
+.s-badge[title^="post edited"] {
+    color: var(--red-500);
+    border-color: var(--red-500);
+}
+
+/* Show a "help" cursor on everything that has a tooltip on hover. */
+.js-admin-dashboard span[title]:hover {
+    cursor: help !important;
+}
+
+
+/* Hide the <p> description to save screen space. */
+.js-dismiss-container .js-title + p {
+   display: none;
+}
+
+/* Compress the flag decline options to save screen space. */
+.js-dismiss-container fieldset > .grid--cell {
+    margin: 3px 0;
+}
+
+/* Make the flag decline reason textbox fill the entire width of the container. */
+.js-dismiss-container input.s-input.js-feedback,
+.js-dismiss-container textarea {
+    width: 100%;
+    max-width: 100% !important;
+}
+
+/* Strip the "helpful" and "decline" buttons from the pop-up menu to save a click. */
+.js-post-flag-options button.s-btn__dropdown.js-resolve-all {
+    display: none;
+}
+.js-post-flag-options button.s-btn__dropdown.js-resolve-all + .s-popover {
+    display: block !important;
+    position: unset !important;
+    background: none;
+    border: none;
+    box-shadow: none;
+    padding: 0;
+    position: unset;
+    min-width: unset;
+    max-width: unset;
+    width: auto !important;
+    transform: none !important;
+    z-index: unset !important;
+}
+.js-post-flag-options button.s-btn__dropdown.js-resolve-all + .s-popover > .js-resolve-action {
+    width: auto !important;
+    margin: 0 8px 0 0;
+    padding: 0;
+}
+
+/* Hide the quick-action buttons that I never use. */
+.js-post-flag-options > div > button {
+    display: none;
+}
+
+
+/* Unknown: */
+.js-admin-dashboard > div.grid--cell {
+    position: relative; /* so the decline + delete option goes over the sidebar */
+    z-index: 1;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+
+    appendStyles();
+    onPageLoad();
+})();
--- SuperShotgun.user.js
+++ SuperShotgun.user.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name         Super Shotgun
 // @description  Facilitates immediate, hassle-free removal of inappropriate questions by a moderator.
-// @version      0.3.2
+// @version      0.3.3
 // @author       Cody Gray
+// @homepage     https://github.com/codygray/so-userscripts
 //
-// @include      https://*stackoverflow.com/*
+// @updateURL    https://github.com/codygray/so-userscripts/raw/master/SuperShotgun.user.js
+// @downloadURL  https://github.com/codygray/so-userscripts/raw/master/SuperShotgun.user.js
+//
+// @include      https://stackoverflow.com/*
+// @include      https://meta.stackoverflow.com/*
 //
 // @exclude      *chat.*
 // @exclude      https://stackoverflow.com/c/*
@@ -14,18 +19,21 @@
     'use strict';
 
     // Moderator check
-    if(typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator ) return;
+    if (typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator) { return; }
 
     const fkey = StackExchange.options.user.fkey;
     const isMeta = (location.hostname == 'meta.stackoverflow.com' || (typeof StackExchange.options.site.parentUrl !== 'undefined'));
 
-    function reloadPage() {
+    function reloadPage()
+    {
         location.reload(true);
     }
 
-    function reloadWhenDone() {
+    function reloadWhenDone()
+    {
         // Triggers when all ajax requests have completed
-        $(document).ajaxStop(function() {
+        $(document).ajaxStop(function()
+        {
             // Stop subsequent calls
             $(this).off("ajaxStop");
 
@@ -33,85 +41,80 @@
         });
     }
 
-    function voteOnPost(pid, voteId) {
-        return new Promise(function(resolve, reject) {
-            if(typeof pid === 'undefined' || pid === null) { reject(); return; }
+    function voteOnPost(pid, voteId)
+    {
+        return new Promise(function(resolve, reject)
+        {
+            if (typeof pid === 'undefined' || pid === null) { reject(); return; }
 
             $.post({
-                url: `https://${location.hostname}/posts/${pid}/vote/${voteId}`,
-                data: {
-                    fkey: fkey
-                }
-            })
+                       url: `https://${location.hostname}/posts/${pid}/vote/${voteId}`,
+                       data: {
+                           fkey: fkey
+                       }
+                   })
             .done(resolve)
             .fail(reject);
         });
     }
 
-    function downvotePost(pid) {
-        return voteOnPost(pid, 3);
-    }
+    function downvotePost(pid) { return voteOnPost(pid, 3); }
 
-    function deletePost(pid) {
-        return voteOnPost(pid, 10);
-    }
+    function deletePost(pid) { return voteOnPost(pid, 10); }
 
-    function undeletePost(pid) {
-        return voteOnPost(pid, 11);
-    }
+    function undeletePost(pid) { return voteOnPost(pid, 11); }
 
     // Close individual post
     // closeReasonId: 'NeedMoreFocus', 'SiteSpecific', 'NeedsDetailsOrClarity', 'OpinionBased', 'Duplicate'
-    // if closeReasonId is 'SiteSpecific', offtopicReasonId : 11-norepro, 13-nomcve, 16-toolrec, 3-custom
-    function closeQuestion(pid, closeReasonId = 'SiteSpecific', offtopicReasonId = 3, offTopicOtherText = 'I’m voting to close this question because ', duplicateOfQuestionId = null) {
-
-        // OffTopic has been replaced with SiteSpecific
-        if(closeReasonId === 'OffTopic') closeReasonId = 'SiteSpecific';
-
-        return new Promise(function(resolve, reject) {
-            if(typeof pid === 'undefined' || pid === null) { reject(); return; }
-            if(typeof closeReasonId === 'undefined' || closeReasonId === null) { reject(); return; }
-            if(closeReasonId === 'SiteSpecific' && (typeof offtopicReasonId === 'undefined' || offtopicReasonId === null)) { reject(); return; }
-
-            if(closeReasonId === 'Duplicate') offtopicReasonId = null;
-
-            // Logging actual action
-            console.log(`%c Closing ${pid} as ${closeReasonId}, reason ${offtopicReasonId}.`, 'font-weight: bold');
+    // if closeReasonId is 'SiteSpecific', then siteSpecificReasonId: 11-norepro, 13-nomcve, 16-toolrec, 3-custom
+    function closeQuestion(pid, closeReasonId, siteSpecificReasonId, siteSpecificOtherText, duplicateId = null)
+    {
+        return new Promise(function(resolve, reject)
+        {
+            if (typeof pid === 'undefined' || pid === null) { reject(); return; }
+            if (typeof closeReasonId === 'undefined' || closeReasonId === null) { reject(); return; }
+            if (closeReasonId === 'SiteSpecific' && (typeof siteSpecificReasonId === 'undefined' || siteSpecificReasonId === null)) { reject(); return; }
+            if (closeReasonId === 'Duplicate' && (typeof duplicateId === 'undefined' || duplicateId === null)) { reject(); return; }
 
             $.post({
-                url: `https://${location.hostname}/flags/questions/${pid}/close/add`,
-                data: {
-                    'fkey': fkey,
-                    'closeReasonId': closeReasonId,
-                    'duplicateOfQuestionId': duplicateOfQuestionId,
-                    'siteSpecificCloseReasonId': offtopicReasonId,
-                    'siteSpecificOtherText': offTopicOtherText,
-                    //'offTopicOtherCommentId': '',
-                    'originalSiteSpecificOtherText': 'I’m voting to close this question because ',
-                }
-            })
+                       url: `https://${location.hostname}/flags/questions/${pid}/close/add`,
+                       data: {
+                           'fkey': fkey,
+                           'duplicateOfQuestionId': (closeReasonId === 'Duplicate' ? duplicateId : null),
+                           'siteSpecificCloseReasonId': (closeReasonId === 'Duplicate' ? null : siteSpecificReasonId),
+                           'siteSpecificOtherText': siteSpecificOtherText,
+                           //'offTopicOtherCommentId': '',
+                           'originalSiteSpecificOtherText': 'I’m voting to close this question because ',
+                       }
+                   })
             .done(resolve)
             .fail(reject);
         });
     }
 
-    function closeQuestionAsOffTopic(pid, offTopicReasonId, offTopicOtherText = '') {
+    function closeQuestionAsOffTopic(pid, offTopicReasonId, offTopicOtherText = '')
+    {
         return closeQuestion(pid, 'SiteSpecific', offTopicReasonId, offTopicOtherText);
     }
 
-    function closeAndRemoveQuestionWithPrejudice(pid, closeReason, offTopicReasonId, offTopicOtherText = '') {
-        return new Promise(function(resolve, reject) {
-            if(typeof pid === 'undefined' || pid == null) { reject(); return; }
+    function closeAndRemoveQuestionWithPrejudice(pid, closeReason, offTopicReasonId, offTopicOtherText = '')
+    {
+        return new Promise(function(resolve, reject)
+        {
+            if (typeof pid === 'undefined' || pid == null) { reject(); return; }
 
-            closeQuestion(pid, closeReason, offTopicReasonId, offTopicOtherText).then(function() {
-                downvotePost(pid).then(function() {
+            closeQuestion(pid, closeReason, offTopicReasonId, offTopicOtherText).then(function()
+            {
+                downvotePost(pid).then(function()
+                {
                     deletePost(pid).then(resolve, reject).finally(reloadPage);
                 }, reject)
             }, reject);
         });
     }
 
-    function appendStyles() {
+    function appendStyles()
+    {
         const styles = `
 <style>
 .js-voting-container {
@@ -129,7 +132,8 @@
         $('body').append(styles);
     }
 
-    function insertNukeButtons() {
+    function insertNukeButtons()
+    {
         const pid = $('.question').data('questionid');
         const qh = $('#question-header + .grid').first();
         if(qh.length == 0) return;
@@ -138,19 +142,19 @@
 
         const nukeActions = $(`<div class="grid--cell ws-nowrap mb8" id="nuke-actions"><span class="fc-light mr2">Nuke As:&nbsp;</span>
                               ${!isMeta ?
-    `<a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="4" title="General computer usage (e.g., Super User)" href="javascript:void(0);">User</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="7" title="Professional server administration (e.g., Server Fault)" href="javascript:void(0);">Server</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="3" data-reasontext="This question does not appear to be about programming within the scope defined in the [Help Center](https://stackoverflow.com/help/on-topic)." title="General off-topic" href="javascript:void(0);">Off-Topic</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="16" title="Soliciting recommendations for off-site resources" href="javascript:void(0);">Recommend</a>`
+    `<a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="4" title="General computer usage (e.g., Super User)" href="javascript:void(0);">User</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="7" title="Professional server administration (e.g., Server Fault)" href="javascript:void(0);">Server</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="3" data-reasontext="This question does not appear to be about programming within the scope defined in the [Help Center](https://stackoverflow.com/help/on-topic)." title="General off-topic" href="javascript:void(0);">Off-Topic</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="16" title="Soliciting recommendations for off-site resources" href="javascript:void(0);">Recommend</a>`
                               :
-    `<a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="6" title="Not about Stack Overflow or the Stack Exchange network" href="javascript:void(0);">Not Meta</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="4" title="Does not appear to seek input and discussion from the community" href="javascript:void(0);">Not Constructive</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="5" title="Rendered obsolete by changes to the system or circumstances" href="javascript:void(0);">No Repro</a>`
+    `<a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="6" title="Not about Stack Overflow or the Stack Exchange network" href="javascript:void(0);">Not Meta</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="4" title="Does not appear to seek input and discussion from the community" href="javascript:void(0);">Not Constructive</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="5" title="Rendered obsolete by changes to the system or circumstances" href="javascript:void(0);">No Repro</a>`
                               }
      <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="NeedsDetailsOrClarity" title="Unclear or requires additional details" href="javascript:void(0);">Unclear</a>
      <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="NeedMoreFocus" title="Not a specific problem with enough detail to identify an adequate answer" href="javascript:void(0);">Too Broad</a>
      <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OpinionBased" title="Primarily opinion-based" href="javascript:void(0);">Opinion</a>
-     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="OffTopic" data-reasonid="3" data-reasontext="This question is off-topic because it is not written in English. All posts on this site are [required to be in English](https://meta.stackexchange.com/questions/13676/)." title="Not written in English" href="javascript:void(0);">Not English</a>
+     <a class="nuke-button ws-nowrap s-btn s-btn__danger s-btn__outlined" data-reason="SiteSpecific" data-reasonid="3" data-reasontext="This question is off-topic because it is not written in English. All posts on this site are [required to be in English](https://meta.stackexchange.com/questions/13676/)." title="Not written in English" href="javascript:void(0);">Not English</a>
 </div>`).appendTo(qh);
 
          nukeActions.one('click', 'a[data-reason]', function() {
@@ -159,7 +163,7 @@
         });
     }
 
-    // On page load
+
     appendStyles();
     insertNukeButtons();
 })();
